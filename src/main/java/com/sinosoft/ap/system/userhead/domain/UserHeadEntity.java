d0c54c2f0f2aea90bc2e910134f02b3135be17a5
Davic Wei <48450976@qq.com>
2017-03-29 20:00:18 +0800

    add USER_HEAD_ADDRESS

--- src/main/java/com/sinosoft/ap/system/userhead/domain/UserHeadEntity.java
+++ src/main/java/com/sinosoft/ap/system/userhead/domain/UserHeadEntity.java
@@ -3,7 +3,14 @@ package com.sinosoft.ap.system.userhead.domain;
 public class UserHeadEntity {
 	private String USER_HEAD_ID;
 	private String USER_ID;
+	private String USER_HEAD_ADDRESS;
 	
+	public String getUSER_HEAD_ADDRESS() {
+		return USER_HEAD_ADDRESS;
+	}
+	public void setUSER_HEAD_ADDRESS(String uSER_HEAD_ADDRESS) {
+		USER_HEAD_ADDRESS = uSER_HEAD_ADDRESS;
+	}
 	public String getUSER_HEAD_ID() {
 		//注释
 		return USER_HEAD_ID;
