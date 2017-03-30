0b0ec6a8a06894dd00f6e289053467bf43b2f638
Davic Wei <48450976@qq.com>
2017-03-30 21:10:18 +0800

    修改manage ap 21.10

--- src/main/java/com/sinosoft/ap/system/resource/domain/ResourceRepository.java
+++ src/main/java/com/sinosoft/ap/system/resource/domain/ResourceRepository.java
@@ -11,7 +11,6 @@ public interface ResourceRepository {
 	 * @param userId
 	 * @return
 	 */
-	ResourceVO selectAll();
 	List<ResourceVO> selectList(ResourceVO resource);
 	void delete(ResourceVO resource);
 	void insert(ResourceVO resource);
