a424ad401c36a4e16f2f16fde1c931a308352a3b
dslsky <416713448@qq.com>
2017-03-30 11:35:22 +0800

    organization第二次修改

--- src/main/java/com/sinosoft/ap/system/organization/domain/OrganizationRepository.java
+++ src/main/java/com/sinosoft/ap/system/organization/domain/OrganizationRepository.java
@@ -2,6 +2,8 @@ package com.sinosoft.ap.system.organization.domain;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface OrganizationRepository {
 	
 	OrganizationVO selectById(String id);
@@ -13,4 +15,12 @@ public interface OrganizationRepository {
 	int insert(OrganizationVO organizationVO);
 	
 	int updateByPrimaryColumn(OrganizationVO organizationVO);
+	
+	int insertOranizationUser(@Param("u_id")String u_id,@Param("o_id")String o_id);
+	
+	int insertOranizationPermisson(@Param("p_id")String p_id,@Param("o_id")String o_id);
+	
+	int removeOrganizationUser(String id);
+	
+	int removeOrganizationPermission(String id);
 }
